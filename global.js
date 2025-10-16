6340c43489fa073e79cf8ca33f5c4de402bafaa0
MaayahGa <gabriel.maayah@gmail.com>
2025-10-16 15:22:36 -0700

    fix tabs
    Removed nav in all html
    instead navigation is js

--- global.js
+++ global.js
@@ -1,21 +1,46 @@
 console.log("IT’S ALIVE!");
 
+
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// Step 2
+// Step 3
 
-let navLinks = $$("nav a");
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "cv/", title: "CV" },
+  { url: "Gabriel Maayah - Resume.pdf", title: "Resume" },
+  { url: "https://github.com/MaayahGa", title: "GitHub" },
+];
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
 
-currentLink?.classList.add("current");
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/portfolio/"; 
 
-nav a.current {
-  font-weight: bold;
-  text-decoration: underline;
-  color: var(--color-accent);
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url.startsWith("http") ? p.url : BASE_PATH + p.url;
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = p.title;
+
+  a.classList.toggle(
+    "current",
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  a.toggleAttribute("target", a.host !== location.host);
+
+
+  nav.append(a);
+
+
+  nav.insertAdjacentText("beforeend", " | ");
 }
