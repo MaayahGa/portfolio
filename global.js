f417b5c3ebe100afa0b4c99571a46e97e57ab17c
MaayahGa <gabriel.maayah@gmail.com>
2025-10-16 15:13:38 -0700

    Update JS
    added mroe features

--- global.js
+++ global.js
@@ -3,3 +3,19 @@ console.log("IT’S ALIVE!");
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// Step 2
+
+let navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+currentLink?.classList.add("current");
+
+nav a.current {
+  font-weight: bold;
+  text-decoration: underline;
+  color: var(--color-accent);
+}
