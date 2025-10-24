0e99fa62b5d1f7c10c7363c073f5a3c1c91df869
MaayahGa <gabriel.maayah@gmail.com>
2025-10-23 21:50:38 -0700

    Add a lot of stuff
    Added github stats latest three projects to home page
    
    just need to add real projects

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
-
-const projectsContainer = document.querySelector('.projects');
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
 async function displayLatestProjects() {
-
-  const projects = await fetchJSON('../lib/projects.json');
-
-
+  const projectsContainer = document.querySelector('.projects');
+  const projects = await fetchJSON('./lib/projects.json');
   const latestProjects = projects.slice(0, 3);
-
   renderProjects(latestProjects, projectsContainer, 'h2');
 }
 
 displayLatestProjects();
+
+async function displayGitHubStats() {
+  const githubData = await fetchGitHubData('MaayahGa'); // Replace with your GitHub username
+  const profileStats = document.querySelector('#profile-stats');
+  
+  if (profileStats) {
+    profileStats.innerHTML = `
+      <dl>
+        <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+        <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+        <dt>Followers:</dt><dd>${githubData.followers}</dd>
+        <dt>Following:</dt><dd>${githubData.following}</dd>
+      </dl>
+    `;
+  }
+}
+
+displayGitHubStats();
