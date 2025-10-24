0f3a0324843a8000e66222d6acd4f0c4f8a12c46
MaayahGa <gabriel.maayah@gmail.com>
2025-10-23 20:49:02 -0700

    Add 1.6

--- projects/projects.js
+++ projects/projects.js
@@ -4,9 +4,9 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
 if (projects && projectsContainer) {
+
   renderProjects(projects, projectsContainer, 'h2');
 
-  // Step 1.6: Count the number of projects
   const title = document.querySelector('.projects-title');
   if (title) {
     title.textContent += ` (${projects.length})`;
