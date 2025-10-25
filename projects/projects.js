3023b586ce64e7e12a5a08a723549658a10503b9
MaayahGa <gabriel.maayah@gmail.com>
2025-10-24 21:00:47 -0700

    hoepfully fix pics

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,11 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
-const projects = await fetchJSON('../lib/projects.json');
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "../"
+    : "/portfolio/";
+
+const projects = await fetchJSON(`${BASE_PATH}lib/projects.json`);
 const projectsContainer = document.querySelector('.projects');
 
 if (projects && projectsContainer) {
